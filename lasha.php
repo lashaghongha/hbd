894b14def70a2a78bdbadd685191a520afe3f202
lashaghongha <135249524+lashaghongha@users.noreply.github.com>
2023-06-18 18:32:45 +0400

    Add files via upload

---
+++ lasha.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Happy Birthday!</title>
+  <style>
+    body {
+      text-align: center;
+      background-color: #f0f0f0;
+      font-family: Arial, sans-serif;
+    }
+    
+    h1 {
+      color: #ff4081;
+    }
+    
+    .hearts {
+      font-size: 60px;
+      color: #ff4081;
+    }
+  </style>
+</head>
+<body>
+  <h1>Happy Birthday!</h1>
+  
+  <p>I hope you will receive my belated greetings.</p>
+  
+  <div class="hearts">
+    <span>&hearts;</span>
+    <span>&hearts;</span>
+    <span>&hearts;</span>
+  </div>
+  
+  <!-- Add photos below this line -->
+  
+  <a href="gallery.php">View Photo Gallery</a>
+  
+  <!-- End of photos -->
+  
+  <script>
+    // You can add JavaScript code here if needed
+  </script>
+</body>
+</html>
